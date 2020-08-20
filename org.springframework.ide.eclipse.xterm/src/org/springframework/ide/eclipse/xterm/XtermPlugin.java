36c62078488ed04e917950cfcb458c12e5542ceb
BoykoAlex <aboyko@pivotal.io>
2020-08-20 13:13:55 -0400

    Font follow eclipse theme. UrlBuilder. Refactoring.

--- org.springframework.ide.eclipse.xterm/src/org/springframework/ide/eclipse/xterm/XtermPlugin.java
+++ org.springframework.ide.eclipse.xterm/src/org/springframework/ide/eclipse/xterm/XtermPlugin.java
@@ -1,10 +1,17 @@
 package org.springframework.ide.eclipse.xterm;
 import java.util.UUID;
 
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.jface.resource.ColorRegistry;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.graphics.RGB;
+import org.eclipse.swt.graphics.RGBA;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.eclipse.ui.themes.ITheme;
 import org.osgi.framework.BundleContext;
+import org.springframework.ide.eclipse.boot.core.SimpleUriBuilder;
 import org.springframework.ide.eclipse.xterm.views.TerminalView;
 import org.springsource.ide.eclipse.commons.livexp.util.ExceptionUtil;
 
@@ -17,6 +24,7 @@ public class XtermPlugin extends AbstractUIPlugin {
 	public static final String SELECTION_COLOR = "org.springframework.ide.eclipse.xterm.selection"; 
 	public static final String CURSOR_COLOR = "org.springframework.ide.eclipse.xterm.cursor"; 
 	public static final String CURSOR_ACCENT_COLOR = "org.springframework.ide.eclipse.xterm.cursorAccent";
+	public static final String FONT = "org.springframework.ide.eclipse.xterm.font";
 	
 	public static final String PREFS_DEFAULT_SHELL_CMD = "org.springframework.ide.eclipse.xterm.defaultShellCmd";
 
@@ -34,8 +42,8 @@ public class XtermPlugin extends AbstractUIPlugin {
 		return plugin;
 	}
 
-	public String getXtermServiceUrl() {
-		return "http://localhost:8080/terminal/";
+	private String getXtermServiceUrl(String terminalId) {
+		return "http://localhost:8080/terminal/" + terminalId;
 	}
 	
 	public static void log(String m, Throwable t) {
@@ -65,5 +73,63 @@ public class XtermPlugin extends AbstractUIPlugin {
 			e.printStackTrace();
 		}
 	}
+	
+	public String xtermUrl(String terminalId, String cmd, String cwd){
+		SimpleUriBuilder urlBuilder = new SimpleUriBuilder(getXtermServiceUrl(terminalId));
+		
+		ITheme theme = PlatformUI.getWorkbench().getThemeManager().getCurrentTheme();
+		ColorRegistry colorRegistry = theme.getColorRegistry();
+		Font font = theme.getFontRegistry().get(XtermPlugin.FONT);
+		
+		urlBuilder.addParameter("bg", rgbToUrlParameter(colorRegistry.get(XtermPlugin.BG_COLOR).getRGB()));
+		urlBuilder.addParameter("fg", rgbToUrlParameter(colorRegistry.get(XtermPlugin.FG_COLOR).getRGB()));
+		RGB selectionColor = colorRegistry.get(XtermPlugin.SELECTION_COLOR).getRGB();
+		// add transparency to selection color
+		urlBuilder.addParameter("selection", rgbaToUrlParameter(new RGBA(selectionColor.red, selectionColor.green, selectionColor.blue, 51)));
+		urlBuilder.addParameter("cursor", rgbToUrlParameter(colorRegistry.get(XtermPlugin.CURSOR_COLOR).getRGB()));
+		urlBuilder.addParameter("cursorAccent", rgbToUrlParameter(colorRegistry.get(XtermPlugin.CURSOR_ACCENT_COLOR).getRGB()));
 
+		urlBuilder.addParameter("fontFamily", font.getFontData()[0].getName());
+		urlBuilder.addParameter("fontSize", String.valueOf(font.getFontData()[0].getHeight()));
+		
+		if (cmd != null && !cmd.isEmpty()) {
+			urlBuilder.addParameter("cmd", cmd);
+		} else {
+			urlBuilder.addParameter("cmd",
+					XtermPlugin.getDefault().getPreferenceStore().getString(XtermPlugin.PREFS_DEFAULT_SHELL_CMD));
+		}
+		if (cwd != null && !cwd.isEmpty()) {
+			urlBuilder.addParameter("cwd", cwd);
+		} else {
+			urlBuilder.addParameter("cwd", ResourcesPlugin.getWorkspace().getRoot().getLocation().toOSString());
+		}
+		
+		return urlBuilder.toString();
+			
+	}
+	
+	private String rgbToUrlParameter(RGB rgb) {
+		StringBuilder sb = new StringBuilder("rgb(");
+		sb.append(rgb.red);
+		sb.append(",");
+		sb.append(rgb.green);
+		sb.append(",");
+		sb.append(rgb.blue);
+		sb.append(")");
+		return sb.toString();
+	}
+
+	private String rgbaToUrlParameter(RGBA rgba) {
+		StringBuilder sb = new StringBuilder("rgba(");
+		sb.append(rgba.rgb.red);
+		sb.append(",");
+		sb.append(rgba.rgb.green);
+		sb.append(",");
+		sb.append(rgba.rgb.blue);
+		sb.append(",");
+		sb.append(rgba.alpha / 255.0);
+		sb.append(")");
+		return sb.toString();
+	}
+	
 }
--- org.springframework.ide.eclipse.xterm/src/org/springframework/ide/eclipse/xterm/views/TerminalView.java
+++ org.springframework.ide.eclipse.xterm/src/org/springframework/ide/eclipse/xterm/views/TerminalView.java
@@ -1,33 +1,19 @@
 package org.springframework.ide.eclipse.xterm.views;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import javax.inject.Inject;
 
-import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.action.IToolBarManager;
-import org.eclipse.jface.action.Separator;
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jface.resource.ColorRegistry;
 import org.eclipse.jface.util.IPropertyChangeListener;
 import org.eclipse.jface.util.PropertyChangeEvent;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.browser.Browser;
-import org.eclipse.swt.graphics.RGB;
-import org.eclipse.swt.graphics.RGBA;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.IActionBars;
-import org.eclipse.ui.ISharedImages;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.part.ViewPart;
@@ -36,8 +22,6 @@ import org.springframework.ide.eclipse.xterm.XtermPlugin;
 
 public class TerminalView extends ViewPart {
 
-	private static final String UTF8 = "UTF8";
-
 	/**
 	 * The ID of the view as specified by the extension.
 	 */
@@ -45,8 +29,7 @@ public class TerminalView extends ViewPart {
 
 	@Inject IWorkbench workbench;
 	
-	private Action action1;
-	private Action action2;
+	private Action refreshAction;
 
 	private Browser browser;
 	
@@ -69,10 +52,8 @@ public class TerminalView extends ViewPart {
 							@Override
 							public IStatus runInUIThread(IProgressMonitor monitor) {
 								try {
-									browser.setUrl(getUrl(createParameters()));
+									refresh();
 									return Status.OK_STATUS;
-								} catch (UnsupportedEncodingException e) {
-									return new Status(IStatus.ERROR, XtermPlugin.getDefault().getBundle().getSymbolicName(), "Failed to refresh terminal", e);
 								} finally {
 									refreshJob = null;
 								}
@@ -80,7 +61,6 @@ public class TerminalView extends ViewPart {
 					 };
 					 refreshJob.schedule();
 				}
-				refresh();
 				break;
 			default:
 			}
@@ -88,10 +68,6 @@ public class TerminalView extends ViewPart {
 
 	};
 
-	public void setUrl(String url) {
-		browser.setUrl(url);
-	}
-	
 	@Override
 	public void createPartControl(Composite parent) {
 		PlatformUI.getWorkbench().getThemeManager().addPropertyChangeListener(PROPERTY_LISTENER);
@@ -102,85 +78,12 @@ public class TerminalView extends ViewPart {
 	}
 	
 	public void refresh() {
-		try {
-			browser.setUrl(getUrl(createParameters()));
-		} catch (UnsupportedEncodingException e) {
-			XtermPlugin.log("Failed to refresh Terminal", e);
-		}
+		browser.setUrl(XtermPlugin.getDefault().xtermUrl(terminalId, null, null));
 	}
 	
-	private String getUrl(Map<String, String> params) {
-		StringBuilder url = new StringBuilder(XtermPlugin.getDefault().getXtermServiceUrl());
-		url.append(terminalId);
-		if (!params.isEmpty()) {
-			url.append("?");
-			List<String> paramStrings = new ArrayList<>(params.size());
-			for (Map.Entry<String, String> e : params.entrySet()) {
-				if (e.getValue() != null && !e.getValue().isEmpty()) {
-					paramStrings.add(e.getKey() + "=" + e.getValue());
-				}
-			}
-			url.append(String.join("&", paramStrings));
-		}
-		return url.toString();
-	}
-	
-	public void startTerminal(String terminalId, String cmd, String cwd) throws Exception {
-		
+	public void startTerminal(String terminalId, String cmd, String cwd) {
 		this.terminalId = terminalId;
-		
-		Map<String, String> params = createParameters();
-		
-		if (cmd != null && !cmd.isEmpty()) {
-			params.put("cmd", URLEncoder.encode(cmd, UTF8));
-		}
-		if (cwd != null && !cwd.isEmpty()) {
-			params.put("cwd", URLEncoder.encode(cwd, UTF8));
-		}
-		
-		browser.setUrl(getUrl(params));
-		
-	}
-	
-	private Map<String, String> createParameters() throws UnsupportedEncodingException {
-		Map<String, String> params = new HashMap<>();
-		ColorRegistry colorRegistry = PlatformUI.getWorkbench().getThemeManager().getCurrentTheme().getColorRegistry();
-		params.put("bg", URLEncoder.encode(rgbToUrlParameter(colorRegistry.get(XtermPlugin.BG_COLOR).getRGB()), UTF8));
-		params.put("fg", URLEncoder.encode(rgbToUrlParameter(colorRegistry.get(XtermPlugin.FG_COLOR).getRGB()), UTF8));
-		RGB selectionColor = colorRegistry.get(XtermPlugin.SELECTION_COLOR).getRGB();
-		// add transparency to selection color
-		params.put("selection", URLEncoder.encode(rgbaToUrlParameter(new RGBA(selectionColor.red, selectionColor.green, selectionColor.blue, 51)), UTF8));
-		params.put("cursor", URLEncoder.encode(rgbToUrlParameter(colorRegistry.get(XtermPlugin.CURSOR_COLOR).getRGB()), UTF8));
-		params.put("cursorAccent", URLEncoder.encode(rgbToUrlParameter(colorRegistry.get(XtermPlugin.CURSOR_ACCENT_COLOR).getRGB()), UTF8));
-		params.put("cmd", URLEncoder.encode(XtermPlugin.getDefault().getPreferenceStore().getString(XtermPlugin.PREFS_DEFAULT_SHELL_CMD), UTF8));
-		params.put("cwd", URLEncoder.encode(ResourcesPlugin.getWorkspace().getRoot().getLocation().toOSString(), UTF8));
-		return params;
-	}
-	
-	
-	
-	private String rgbToUrlParameter(RGB rgb) throws UnsupportedEncodingException {
-		StringBuilder sb = new StringBuilder("rgb(");
-		sb.append(rgb.red);
-		sb.append(",");
-		sb.append(rgb.green);
-		sb.append(",");
-		sb.append(rgb.blue);
-		sb.append(")");
-		return sb.toString();
-	}
-
-	private String rgbaToUrlParameter(RGBA rgba) throws UnsupportedEncodingException {
-		StringBuilder sb = new StringBuilder("rgba(");
-		sb.append(rgba.rgb.red);
-		sb.append(",");
-		sb.append(rgba.rgb.green);
-		sb.append(",");
-		sb.append(rgba.rgb.blue);
-		sb.append(",");
-		sb.append(rgba.alpha / 255.0);
-		sb.append(")");
-		return sb.toString();
+		browser.setUrl(XtermPlugin.getDefault().xtermUrl(terminalId, cmd, cwd));
 	}
 	
 	private void contributeToActionBars() {
@@ -190,43 +93,22 @@ public class TerminalView extends ViewPart {
 	}
 
 	private void fillLocalPullDown(IMenuManager manager) {
-		manager.add(action1);
-		manager.add(new Separator());
-		manager.add(action2);
+		manager.add(refreshAction);
 	}
 
 	private void fillLocalToolBar(IToolBarManager manager) {
-		manager.add(action1);
-		manager.add(action2);
+		manager.add(refreshAction);
 	}
 
 	private void makeActions() {
-		action1 = new Action() {
+		refreshAction = new Action() {
 			public void run() {
-				showMessage("Action 1 executed");
+				refresh();
 			}
 		};
-		action1.setText("Action 1");
-		action1.setToolTipText("Action 1 tooltip");
-		action1.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().
-			getImageDescriptor(ISharedImages.IMG_OBJS_INFO_TSK));
-		
-		action2 = new Action() {
-			public void run() {
-				showMessage("Action 2 executed");
-			}
-		};
-		action2.setText("Action 2");
-		action2.setToolTipText("Action 2 tooltip");
-		action2.setImageDescriptor(workbench.getSharedImages().
-				getImageDescriptor(ISharedImages.IMG_OBJS_INFO_TSK));
-	}
-
-	private void showMessage(String message) {
-		MessageDialog.openInformation(
-			browser.getShell(),
-			"Sample View",
-			message);
+		refreshAction.setText("Refresh");
+		refreshAction.setToolTipText("Refresh Terminal");
+		refreshAction.setImageDescriptor(XtermPlugin.imageDescriptorFromPlugin(XtermPlugin.getDefault().getBundle().getSymbolicName(), "icons/refresh.png"));		
 	}
 
 	@Override
@@ -238,6 +120,5 @@ public class TerminalView extends ViewPart {
 	public void dispose() {
 		PlatformUI.getWorkbench().getThemeManager().removePropertyChangeListener(PROPERTY_LISTENER);
 	}
-	
 	
 }
